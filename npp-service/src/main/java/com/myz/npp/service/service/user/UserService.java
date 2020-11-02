c3a6c42096b7fe8846a593848508684f9f628067
yzMa <2641007740@qq.com>
2020-11-02 21:54:09 +0800

    sharding jdbc yml config

--- npp-service/src/main/java/com/myz/npp/service/service/user/UserService.java
+++ npp-service/src/main/java/com/myz/npp/service/service/user/UserService.java
@@ -2,9 +2,13 @@ package com.myz.npp.service.service.user;
 
 import com.myz.npp.service.dao.mapper.UserMapper;
 import com.myz.npp.service.dao.model.User;
+import com.myz.npp.service.dao.model.UserExample;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
 
 /**
  * @author yzMa
@@ -21,12 +25,27 @@ public class UserService {
 
 
     /**
-     * 根据id查询User
+     * 根据id查询User，
+     * 使用shardingJdbc没有使用分片键所有库所有表查询
      * @param id
      * @return
      */
     public User getById(Long id){
         return userMapper.selectByPrimaryKey(id);
+    }
+
+    /**
+     * 使用shardingJdbc根据分片键 路由到指定库指定表
+     * @param userId
+     * @return
+     */
+    public User getByShardingColumn(String userId){
+        UserExample example = new UserExample();
+        UserExample.Criteria criteria = example.createCriteria();
+        criteria.andUserIdEqualTo(userId);
+        List<User> users = userMapper.selectByExample(example);
+
+        return CollectionUtils.isEmpty(users)?null:users.get(0);
     }
 
 }
--- npp-service/src/test/java/com/myz/npp/service/service/UserServiceTest.java
+++ npp-service/src/test/java/com/myz/npp/service/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.myz.npp.service.service;
 import com.myz.npp.service.NppServiceStartApp;
 import com.myz.npp.service.dao.model.User;
 import com.myz.npp.service.service.user.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @date 2020/11/1 1:08 下午
  * @email 2641007740@qq.com
  */
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = NppServiceStartApp.class)
 public class UserServiceTest {
@@ -26,6 +28,18 @@ public class UserServiceTest {
     public void getById(){
         Long id = 1L;
         User user = userService.getById(id);
+        log.info("none sharding column query user={}",user==null?"":user);
+    }
 
+    @Test
+    public void getByShardingColumn(){
+        String userId = "userId111111";
+
+        int ds = userId.hashCode() % 2;
+        int table = userId.hashCode()%3;
+        log.info("ds{},table{}",ds,table);
+
+        User user = userService.getByShardingColumn(userId);
+        log.info("sharding column query user={}",user==null?"":user);
     }
 }
